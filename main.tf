6acc4edde94bfe7b32278901fbb60096d7b79602
amitkumardube <amitkumardube@gmail.com>
2021-02-17 20:44:12 +0200

    updated file for gcp

--- main.tf
+++ main.tf
@@ -1,56 +1,47 @@
 terraform {
+  backend "remote" {
+     organization = "terraform-associate-certification"
+     workspaces {
+       name = "gh-actions-demo"
+     }
+   }
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "3.5.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
+  }
+}
+
+provider "google" {
+
+  credentials = file("./my-first-project-298218-a7316b8c9560.json")
+
+  project = "my-first-project-298218"
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  //depends_on = [google_storage_bucket.example_bucket]
+  name         = var.instance_name
+  machine_type = var.instance_type
+  tags = var.instance_tags
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
     }
   }
-  required_version = "~> 0.14"
 
-  backend "remote" {
-    organization = "terraform-associate-certification"
-
-    workspaces {
-      name = "gh-actions-demo"
+  network_interface {
+    network = "default"
+    access_config {
     }
   }
-}
 
-
-provider "aws" {
-  region = "us-west-2"
-}
-
-
-
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  provisioner "local-exec"{
+    command = "echo ${google_compute_instance.vm_instance.name} : ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
   }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
 }
