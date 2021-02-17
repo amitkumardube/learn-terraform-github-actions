17ad219e1151eb89b5fa69c98354b07bfe9ab1e8
amitkumardube <amitkumardube@gmail.com>
2021-02-17 20:56:51 +0200

    updated file for gcp

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "instance_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "instance_tags" {
+  type = list(any)
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
