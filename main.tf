4cce218bd210ee057d9d76d5f3f1af3753e80118
amitkumardube <37042351+amitkumardube@users.noreply.github.com>
2021-02-17 20:29:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "terraform-associate-certification"
 
     workspaces {
       name = "gh-actions-demo"
